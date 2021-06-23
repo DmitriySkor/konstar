c3bc1ccae580ee08bf8599d6921025d04e9f2ec3
DmitriySkor <rionskey@gmail.com>
2021-06-24 00:02:49 +0300

    add page - contact, about, half service

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
+// menu
+
+jQuery(document).ready(function($) {
+    $('.submenu > li').matchHeight();
+  });
+
+
 // Инициализация owlCarousel
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel(
@@ -27,11 +34,11 @@ $(document).ready(function () {
 // поле для выбора файла <input type="file">.
 // поле для ввода чисел <input type="number">.
 // раскрывающийся список <select>;
-(function ($) {
-    $(function () {
-        $('input, select').styler();
-    });
-})(jQuery);
+// (function ($) {
+//     $(function () {
+//         $('input, select').styler();
+//     });
+// })(jQuery);
 
 // Инициализация Fancybox
 $().fancybox({
@@ -63,8 +70,78 @@ $(function() {
         });
 });
 
-// menu
+// $("#example-basic").steps({
+//   headerTag: "h3",
+//   bodyTag: "section",
+//   transitionEffect: "slideLeft",
+//   autoFocus: true
+// });
 
-jQuery(document).ready(function($) {
-    $('.submenu > li').matchHeight();
+$(document).ready(function(){
+
+  var current_fs, next_fs, previous_fs; //fieldsets
+  var opacity;
+  
+  $(".next").click(function(){
+  
+  current_fs = $(this).parent();
+  next_fs = $(this).parent().next();
+  
+  //Add Class Active
+  $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+  
+  //show the next fieldset
+  next_fs.show();
+  //hide the current fieldset with style
+  current_fs.animate({opacity: 0}, {
+  step: function(now) {
+  // for making fielset appear animation
+  opacity = 1 - now;
+  
+  current_fs.css({
+  'display': 'none',
+  'position': 'relative'
+  });
+  next_fs.css({'opacity': opacity});
+  },
+  duration: 600
+  });
+  });
+  
+  $(".previous").click(function(){
+  
+  current_fs = $(this).parent();
+  previous_fs = $(this).parent().prev();
+  
+  //Remove class active
+  $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+  
+  //show the previous fieldset
+  previous_fs.show();
+  
+  //hide the current fieldset with style
+  current_fs.animate({opacity: 0}, {
+  step: function(now) {
+  // for making fielset appear animation
+  opacity = 1 - now;
+  
+  current_fs.css({
+  'display': 'none',
+  'position': 'relative'
+  });
+  previous_fs.css({'opacity': opacity});
+  },
+  duration: 600
+  });
+  });
+  
+  $('.radio-group .radio').click(function(){
+  $(this).parent().find('.radio').removeClass('selected');
+  $(this).addClass('selected');
+  });
+  
+  $(".submit").click(function(){
+  return false;
+  })
+  
   });
